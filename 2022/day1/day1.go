0e6a9c6243e0d0690218807a4c1ca0c47e6f82c0
Geraud <geraud.masson@ext.adeo.com>
2022-12-02 22:21:36 +0100

    day1 done

---
+++ 2022/day1/day1.go
@@ -0,0 +1,55 @@
+package main
+	
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+
+    readFile, err := os.Open("input.txt")
+    check(err)
+
+    fileScanner := bufio.NewScanner(readFile)
+    fileScanner.Split(bufio.ScanLines)
+
+    var numberOfCalories = 0
+    var (
+        firstElve int = 0
+        secondElve    = 0
+        thirdElve    = 0
+    )
+
+    for fileScanner.Scan() {
+        if fileScanner.Text() == "" {
+            if numberOfCalories > firstElve {
+                thirdElve = secondElve
+                secondElve = firstElve
+                firstElve = numberOfCalories
+            } else if numberOfCalories > secondElve{
+                thirdElve = secondElve
+                secondElve = numberOfCalories
+            } else if numberOfCalories > thirdElve{
+                thirdElve = numberOfCalories
+            }
+            numberOfCalories = 0
+        } else {
+            i, err := strconv.Atoi(fileScanner.Text())
+            check(err)
+            numberOfCalories += i
+        }
+    }
+
+    fmt.Println("Top three Elves : ", firstElve, secondElve, thirdElve)
+    fmt.Println("Total calories : ", firstElve + secondElve + thirdElve)
+
+    readFile.Close()
+}
---
+++ 2022/go.mod
@@ -0,0 +1,3 @@
+module github.com/garywald/adventofcode
+
+go 1.18
